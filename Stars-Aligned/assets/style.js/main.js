ede0c8779b22c0635783ae2b8d44e76d844a4960
rootcanopy <dave.trees201@gmail.com>
2020-03-20 09:38:09 +0100

    continuing with styling and will hopefully have started the second api by afternoon

--- Stars-Aligned/assets/style.js/main.js
+++ Stars-Aligned/assets/style.js/main.js
@@ -1,3 +1,4 @@
+// api call for pic of day 
 const apodURL = "https://api.nasa.gov/planetary/apod?api_key=";
 const api_key = "aTzWAFGW6diC9Gmiv2motIrgf68tuKJyXiXxQ8IL";
 let data; 
@@ -15,8 +16,8 @@ function api_call() {
     };
     xhr.send();
 }
-
-function showData() {
+// TODO
+function showData() { 
     document.getElementById("title").innerHTML = data.title;
     document.getElementById("date").innerHTML = data.date;
     document.getElementById("pic").src = data.hdurl;
@@ -24,30 +25,7 @@ function showData() {
 }
 
 document.getElementById("myBtn").addEventListener("click", showData);
-api_call(); 
-
-//adding some effects                           /* To Do, All this code needs to be checked/ debugged */
-let modal = document.getElementById('ModalA');
-let btn = document.getElementById('myBtn');
-let closebtn = document.getElementsByClassName('btn-close-apod');
-
-btn.onclick = function () {
-    modal.style.display = "block";
-};
-
-function closeAPOD() {
-    document.getElementById("ModalA").style.display = "none";
-}
-
-/*btn.onclick = function () {
-    modal.style.display = "none";
-};*/
-//Click outside of window for modal APOD to close
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+api_call();
 
 //this is the api request for nasa library
 const searchUrl = "https://images-api.nasa.gov/";
@@ -61,25 +39,25 @@ function searchNasa() {
     let URL = searchUrl + query + Input;
 
     xhr.open("GET", URL, true);
+
     xhr.onreadystatechange = function () {
+
         if (this.readyState === 4 && this.status === 200) {
             Library(JSON.parse(this.responseText));
-
         }
     };
-
     document.getElementById("nasaLib").style.display = "block";
     xhr.send();
 }
+// sending search results to the DOM
 // @ts-check
 function Library(result) {
-    //$(".nasaImg").empty();
 
     for (let i = 0; i < result.collection.items.length; i++) {
         document.getElementById("nasaImg").innerHTML += '<div class="response"><div class="images"><a target="_blank" href="' 
         + result.collection.items[i].links[0].href + '"><img class="item" width="600" height="600" src="' 
         + result.collection.items[i].links[0].href + '"></a></<div></div>';
-    } // 
+    }
 };
 
 // Get the input field
@@ -96,7 +74,37 @@ input.addEventListener("keyup", function (event) {
     }
 });
 
-// library modal events
 function closeLibrary() {
     document.getElementById("nasaLib").style.display = "none";
 }
+/////////////////////////////////////////
+const modal = document.getElementsByClassName("modal");
+const btn = document.getElementById('myBtn');
+
+// library modal events
+function closeBtn() {
+    document.getElementById("close-apod").style.display = "none";
+}
+
+
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+/*btn.onclick = function closeBtn() {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+//adding some effects 
+
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+
+//Click outside of window for modal APOD to close
+*/
