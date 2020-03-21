7a77b7405af78b9b24d4205bd0f74761494b5c44
rootcanopy <dave.trees201@gmail.com>
2020-03-21 18:24:15 +0100

    working with the new api call

--- assets/js/astro.js
+++ assets/js/astro.js
@@ -1,6 +1,6 @@
 const api_key = "&9d8a5fd659f1b00e1180351a407736728fd10b55";
 const api_secret = "caa9162960edeb25866241f41b741ee4086ae72f";
 
-"https://astrobin.com/api/v1/.../?...&api_key=123456790&api_secret=0987654321&format=json";
+//"https://astrobin.com/api/v1/.../?...&api_key=123456790&api_secret=0987654321&format=json";
 
 fetch('https://astrobin.com/api/v1/?imageoftheday/?limit=1')
