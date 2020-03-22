3bc4a21bda4228d5ad09bc96ae9db643512fe770
rootcanopy <dave.trees201@gmail.com>
2020-03-22 13:54:08 +0100

    map is working well, need to fix the modal and a couple of console errors.

--- assets/js/astro.js
+++ assets/js/astro.js
@@ -1,23 +1,22 @@
-var mymap = L.map("theMap").setView([0, 0], 2);
-let attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
+//MAKING A MAP AND TILES
+var mymap = L.map("myEarth").setView([0, 0], 2);
+let attribution = '&copy; <a href="https://www.openstreetmap.org/copyright>OpenStreetMap</a> contributors';
 
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const tiles = L.tileLayer(tileUrl, { attribution });
     
 
 tiles.addTo(mymap);
-/*
-// Making a marker with a custom icon
-const issIcon = L.icon({
-    iconUrl: 'iss200.png',
-    iconSize: [50, 32],
+
+// MAKING A MARKER WITH CUSTOM ICON
+let myIcon = L.icon({
+    iconUrl: "assets/pic/thefloater.png",
+    iconSize: [50, 30],
     iconAnchor: [25, 16]
 });
-const marker = L.marker([0, 0], {
-    icon: issIcon
-}).addTo(mymap);
-*/
+
+const marker = L.marker([0, 0], {icon: myIcon}).addTo(mymap);
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 /*let firstTime = true;*/
@@ -26,21 +25,21 @@ async function getISS() {
     const response = await fetch(api_url);
     const data = await response.json();
      const { latitude, longitude } = data;
-        
-    document.getElementById('lat').textContent = latitude;//;toFixed(2)
-    document.getElementById('lon').textContent = longitude;//.toFixed(2
-    
-        console.log(latitude);
-        console.log(longitude);
-}  
+
+    marker.setLatLng([latitude, longitude]);
+    mymap.setView([latitude, longitude], 3);
+    document.getElementById('lat').textContent = latitude.toFixed(2);
+    document.getElementById('lon').textContent = longitude.toFixed(2);    
+        /*console.log(latitude);
+        console.log(longitude);*/
+ 
         /*marker.setLatLng([latitude, longitude]);
     if (firstTime) {
-        mymap.setView([latitude, longitude], 2);
+        
         firstTime = false;
-    }
-    
-}
-console.log('lat');*/
+    }*/
+}   
+
 getISS();
 
-setInterval(getISS, 1000);
+setInterval(getISS, 20000);
